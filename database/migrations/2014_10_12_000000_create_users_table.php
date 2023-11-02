0b715211c86e5577e08e8005e14b07fa31c96979
José Mejia <josemejia.cto@gmail.com>
2023-11-02 12:01:47 -0600

    Tabla de usuarios hecha en database/migrations con los campos necesarios

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,9 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->integer('telefono');
+            $table->string('correo')->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
